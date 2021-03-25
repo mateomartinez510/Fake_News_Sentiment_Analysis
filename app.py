1bbe24495db1d1f10645c8278a70dcd7a51c6bdc
mateomartinez510 <60012996+mateomartinez510@users.noreply.github.com>
2021-03-25 14:17:23 -0700

    Update app.py
    Updating API endpoint

--- app.py
+++ app.py
@@ -114,6 +114,8 @@ def submit():
 
 #####
 
+##disabling SQL to test app in local enviornment
+
     # saving data to SQL database
 
     #db_file = "database/fake_news_database.db"
@@ -170,23 +172,110 @@ def submit():
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 
 def api_prediction():
-    if not request.json:
+
+    if not request.json: #
         abort(400)
 
 
+    data = request.json
+    news_article_URL = data['URL']
+    user_input_prediction = data['UserPrediction']
 
-    data = request.args
-    foo = request.json
-    return data, foo
+
+    # pulling html from webite with
+    html = requests.get(news_article_URL).text
+    text = fulltext(html)
 
 
-    # task = {
-    #     'id': tasks[-1]['id'] + 1,
-    #     'title': request.json['title'],
-    #     'description': request.json.get('description', ""),
-    #     'done': False
-    # }
-    #return jsonify({'task': task}), 201
+    # loading TF-IDF Vectorizer weights:
+    model_dir = 'pkl_files/'
+    filename = 'tfidf_vectorizer.sav'
+    tfidf_vectorizer = joblib.load(model_dir + filename)
+
+
+    # transforming the news article into TF-IDF Matrix:
+    X_test = tfidf_vectorizer.transform([text])
+    
+
+    # loading all five models
+    
+    # loading Naive Bayes Model:
+    filename = 'tfidf_nb_classifier.sav'
+    tfidf_nb_classifier = joblib.load(model_dir + filename)
+    nb_pred = tfidf_nb_classifier.predict(X_test)[0]
+    nb_proba = tfidf_nb_classifier.predict_proba(X_test)[0][1]
+
+    # loading the TF-IDF Logistic Regression Model:
+    filename = 'tfidf_lr_classifier.sav'
+    tfidf_lr_classifier = joblib.load(model_dir + filename)
+    lr_pred = tfidf_lr_classifier.predict(X_test)[0]
+    lr_proba = tfidf_lr_classifier.predict_proba(X_test)[0][1]
+
+    # loading the TF-IDF Random Forest Model:
+    filename = 'tfidf_rf_classifier.sav'
+    tfidf_rf_classifier = joblib.load(model_dir + filename)
+    rf_pred = tfidf_rf_classifier.predict(X_test)[0]
+    rf_proba = tfidf_rf_classifier.predict_proba(X_test)[0][1]
+
+    # loading the TF-IDF Passive Aggressive Classification Model:
+    filename = 'tfidf_pac_classifier.sav'
+    tfidf_pac_classifier = joblib.load(model_dir + filename)
+    pac_pred = tfidf_pac_classifier.predict(X_test)[0]
+    pac_pdf = tfidf_pac_classifier.decision_function(X_test)[0]
+    pac_proba = stats.norm.cdf(pac_pdf)
+
+    # loading the TF-IDF Support Vector Machine Model:
+    filename = 'tfidf_svm_classifier.sav'
+    tfidf_svm_classifier = joblib.load(model_dir + filename)
+    svm_pred = tfidf_svm_classifier.predict(X_test)[0]
+    svm_pdf = tfidf_svm_classifier.decision_function(X_test)[0]
+    svm_proba = stats.norm.cdf(svm_pdf)
+    
+    # this counter with track the overall truthiness of the models:
+    fake_news_counter = 0
+    
+    if nb_pred == "fake":
+        fake_news_counter += 1
+    if lr_pred == "fake":
+        fake_news_counter += 1    
+    if rf_pred == "fake":
+        fake_news_counter += 1    
+    if pac_pred == "fake":
+        fake_news_counter += 1    
+    if svm_pred == "fake":
+        fake_news_counter += 1
+
+    # this variable with be used for logging predictions for the SQL database
+    algorithm_pred = ""
+
+    if fake_news_counter <= 3:
+          conclusion = "Based on these results, the majority of the models determined this to be REAL news!"
+          algorithm_pred = "real"
+    else:
+          conclusion = "Based on these results, the majority of the models determined this to be FAKE news!" 
+          algorithm_pred = "fake"   
+              
+
+
+    output = {"url_submitted" : news_article_URL, "article_body" : text, 
+                            "user_input_prediction":user_input_prediction, 
+                            "nb_pred":nb_pred, "nb_proba":round(nb_proba,2), 
+                            "lr_pred":lr_pred, "lr_proba":round(lr_proba,2), 
+                            "rf_pred":rf_pred, "rf_proba":round(rf_proba,2), 
+                            "pac_pred":pac_pred, "pac_pdf":round(pac_proba,2), 
+                            "svm_pred":svm_pred, "svm_pdf":round(svm_proba,2), 
+                            "fake_news_counter":fake_news_counter, 
+                            "conclusion" : conclusion,
+                            "algorithm_pred" : algorithm_pred  
+
+             }
+
+
+    return output, 201
+
+
+
+
 
 
 
